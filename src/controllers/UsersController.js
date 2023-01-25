bd0cf89910c2f7cdc0f05eec619bff7c63f10a2a
Jessica Ranft <jessica.ranft@gmail.com>
2023-01-25 11:01:31 -0300

    added user name, email and password UPDATE function

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -18,6 +18,54 @@ class UsersController {
 
       return res.status(201).json();
   }
+
+  async update(req, res) {
+    const { name, email, password, old_password } = req.body;
+    const { id } = req.params;
+
+    const database = await sqliteConnection();
+
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user) {
+      throw new AppError("Esse usuário não existe!");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id === user.id) {
+      throw new AppError("Já existe um usuário com esse e-mail.");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !old_password) {
+      throw new AppError("Você precisa digitar sua senha antiga.");
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
+
+    return res.json();
+  }
 }
 
 module.exports = UsersController;
